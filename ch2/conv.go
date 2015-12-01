5ba6b93ec40476b806986351e0ad4fc529e100f1
Kartik Singhal <kartiksinghal@gmail.com>
2015-11-30 21:05:35 -0500

    tempconv package put to use

--- ch2/conv.go
+++
@@ -1,8 +0,0 @@
-package tempconv
-
-func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
-func CtoK(c Celsius) Kelvin     { return Kelvin(c - AbsoluteZeroC) }
-func KtoC(k Kelvin) Celsius     { return Celsius(k - FreezingK) }
-func FtoK(f Fahrenheit) Kelvin  { return CtoK(FtoC(f)) }
-func KtoF(k Kelvin) Fahrenheit  { return CtoF(KtoC(k)) }
